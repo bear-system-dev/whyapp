43afd3ed67390ec45b35c8b324c486a1aa08b625
LestterX <lgs.alves@hotmail.com>
2023-11-16 17:40:11 -0300

    feat: logout done

--- backend/src/server/database/providers/blackListedToken/GetByToken.ts
+++ backend/src/server/database/providers/blackListedToken/GetByToken.ts
@@ -2,13 +2,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getByToken = async (token: string) => {
+  if (token.includes('Bearer')) { token = token.replace('Bearer ', ''); }
   try {
     const blackListedToken = await prisma.blackListedToken.findFirst({
       where: {
         token: token,
       }
     });
-    return blackListedToken?.token;
+    return blackListedToken?.id;
   } catch (error) {
     console.log(error);
     return new Error('An error ocurred verifying token');
--- backend/src/server/shared/middlewares/VerifyToken.ts
+++ backend/src/server/shared/middlewares/VerifyToken.ts
@@ -1,15 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { services } from '../services';
+import { blackListedToken } from '../../database/providers/blackListedToken';
 
-export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers['authorization'];
-  console.log(token);
 
   if (!token) return res.status(StatusCodes.UNAUTHORIZED).json({
     message: 'No authorization token found',
     status: 401
   });
+
+  const blackListedTokenResult = await blackListedToken.getByToken(token);
+  if (typeof blackListedTokenResult === 'number') { 
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'Expired Token. Please log in',
+      position: blackListedTokenResult,
+      status: 401
+    });
+  }
 
   const decoded = services.jwt.verifyToken(token);
   if (decoded instanceof Error) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
